c48b7bf0bb818f715edbf50c1ce9e3a83511de02
jmuldvp <jmul.dvp@gmail.com>
2017-11-30 19:26:24 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+import ToDo from './components/ToDo/ToDo.js';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <ul>
+          <ToDo />
+          <ToDo />
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
